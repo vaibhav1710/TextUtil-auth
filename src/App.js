6a2814a4c9ab8ebf052572aff8b0dc1afa781864
Vaibhav Singh <singhvaibhav1710@gmail.com>
2023-03-13 00:13:28 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+ import TextForm from './components/TextForm';
+ 
 
 function App() {
+  const [mode,setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey'
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
    <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+   <Navbar  title = "TextUtils" mode={mode} link="Link"  toggleMode = {toggleMode}/>
+   <div className='container' my-3='true'>
+    <TextForm mode = {mode} heading = "Enter The Text Here"/> 
+    {/* <About/>  */}
+   </div>
    </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+
+    const handleUCase = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleLCase = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    // to show the typed character
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    const handleCopy = () => {
+        var text = document.getElementById("exampleFormControlTextarea1");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const[text,setText] = useState('');
+    // text = "new Text"; // wrong way to change the text
+    // setText = ("new Text");
+    return (
+        <>
+    <div className='container' style = {{color:props.mode==='light'?'black':'white'}}>
+    <h1>{props.heading}</h1>
+<div className="mb-3" >
+  <textarea className="form-control" value ={text} style = {{backgroundColor: props.mode==='light'?'white':'grey' , color:props.mode==='light'?'black':'white'}} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="14"></textarea>
+</div>
+<button className="btn-btn-primary mx-2" onClick={handleUCase}>Convert to UpperCase</button>
+<button className="btn-btn-primary mx-2" onClick={handleLCase}>Convert to LowerCase</button>
+<button className="btn-btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+    </div>
+    <div className='container my-3' style = {{color:props.mode==='light'?'black':'white'}}>
+        <h2>Your text Summary</h2>
+        <p>{text.trim(text.split(" ").length)} words, {text.length} characters</p>
+        <p>{0.008*(text.split(" ").length)} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+}
